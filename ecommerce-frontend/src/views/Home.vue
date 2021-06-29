65b0b2d63b455032074f2a92516c1d6670bd367d
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-06-28 23:50:31 -0600

    Styles were added to the home page.

--- ecommerce-frontend/src/views/Home.vue
+++ ecommerce-frontend/src/views/Home.vue
@@ -1,16 +1,55 @@
 <template>
-  <hello-world />
+  <v-sheet color="primary">
+    <v-layout
+      wrap
+      align-center
+      justify-center
+      class="px-5"
+      style="min-height: 100vh"
+    >
+      <v-flex xs12 sm8 md6 class="px-md-5">
+        <div class="white--text">
+          <h1 class="my-3">
+            <strong>Administrar tu negocio nunca habia sido tan facil.</strong>
+          </h1>
+          <p>
+            Accede ya a nuestro servicio, y obtiene todos tus articulos a la
+            puerta de tu casa. Create una cuenta, si aun no lo has hecho.
+          </p>
+        </div>
+        <br />
+        <v-btn rounded large class="primary--text ma-2">
+          <v-icon class="mr-2">mdi-account</v-icon>
+          <span>Crear una cuenta</span>
+        </v-btn>
+        <v-btn text color="white" large class="ma-2">
+          <v-icon class="mr-2">mdi-login</v-icon>
+          <span>Iniciar sesion</span>
+        </v-btn>
+      </v-flex>
+      <v-flex xs12 sm8 md6 class="text-center">
+        <img src="../assets/banner.svg" width="80%" alt="" />
+      </v-flex>
+    </v-layout>
+  </v-sheet>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "../components/HelloWorld.vue";
+import Component from "vue-class-component";
 
-export default Vue.extend({
-  name: "Home",
-
-  components: {
-    HelloWorld,
-  },
-});
+@Component
+export default class HomePage extends Vue {}
 </script>
+
+<style scoped>
+@media screen and (min-width: 1000px) {
+  h1 {
+    font-size: 40px;
+  }
+
+  p {
+    font-size: 20px;
+  }
+}
+</style>
