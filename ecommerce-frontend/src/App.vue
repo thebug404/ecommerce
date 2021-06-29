c87d299a0d204303c634c63b1e3f27deec793a49
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-06-28 23:00:25 -0600

    Add frontend

---
+++ ecommerce-frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+
+    <main>
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class App extends Vue {}
+</script>
